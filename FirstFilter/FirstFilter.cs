b8e8c82e3a4d8fde7468232af97a57a98eff1857
gu1show <denissolovyov57@gmail.com>
2022-05-10 23:59:27 +0300

    Checking algorithms

--- FirstFilter/FirstFilter.cs
+++ FirstFilter/FirstFilter.cs
@@ -1,6 +1,8 @@
-﻿using OpenCvSharp;
-using System;
+﻿using System;
 using System.Drawing;
+using System.IO;
+using OpenCvSharp;
+using ClosedXML.Excel;
 
 namespace FirstFilter
 {
@@ -10,92 +12,115 @@ namespace FirstFilter
         {
             //const int width = 1024, height = 512;
             SolidBrush brush = new SolidBrush(Color.White);
+            IXLWorkbook newExcelFile = new XLWorkbook();
+            IXLWorksheet sheet = newExcelFile.Worksheets.Add("First filter");
+            sheet.Cell("A1").Value = "Image";
+            sheet.Cell("B1").Value = "Accuracy";
+            sheet.Cell("C1").Value = "F1";
+            sheet.Cell("D1").Value = "Time";
 
-            using (var gray = Cv2.ImRead(@"absolute path", ImreadModes.Grayscale))
+            int row = 2;
+            for (short id = 11; id < 1539; id++)
             {
-                Mat image = Cv2.ImRead(@"absolute path", ImreadModes.Color);
-
-                Cv2.Resize(image, image, new OpenCvSharp.Size(1024, 512));
-                Cv2.Resize(gray, gray, new OpenCvSharp.Size(1024, 512));
-
-                using (new Window("image", image, WindowFlags.AutoSize))
+                if (File.Exists($@"C:\Users\Denis\source\repos\Алгоритмы для курсовой работы\algorithms\Images\Train\img{id}.jpg"))
                 {
-                    Cv2.WaitKey();
-                }
-                using (new Window("gray image", gray, WindowFlags.AutoSize))
-                {
-                    Cv2.WaitKey();
-                }
-
-                Mat blured = new Mat();
-                Cv2.GaussianBlur(gray, blured, new OpenCvSharp.Size(17, 17), 0);
-                using (new Window("blured image", blured, WindowFlags.AutoSize))
-                {
-                    Cv2.WaitKey();
-                }
-
-                Mat threshold = new Mat();
-                Cv2.AdaptiveThreshold(blured, threshold, 255,
-                                      AdaptiveThresholdTypes.GaussianC,
-                                      ThresholdTypes.Binary, 5, 0);
-                using (new Window("adaptive threshold", threshold, WindowFlags.AutoSize))
-                {
-                    Cv2.WaitKey();
-                }
-
-                Mat kernel = new Mat();
-                Cv2.GetStructuringElement(MorphShapes.Rect, new OpenCvSharp.Size(5, 5));
-                Mat dilate = new Mat();
-                Cv2.Dilate(threshold, dilate, kernel);
-                using (new Window("dilation", dilate, WindowFlags.AutoSize))
-                {
-                    Cv2.WaitKey();
-                }
-
-                OpenCvSharp.Point[][] contours = new OpenCvSharp.Point[1][];
-                HierarchyIndex[] temp = new HierarchyIndex[1];
-                Cv2.FindContours(dilate, out contours, out temp,
-                                 RetrievalModes.External,
-                                 ContourApproximationModes.ApproxSimple);
-
-                Bitmap tempImage = new Bitmap(gray.Width, gray.Height);
-                for (int i = 0; i < gray.Width; i++)
-                    for (int j = 0; j < gray.Height; j++)
-                        tempImage.SetPixel(i, j, Color.Black);
-
-                foreach (var contour in contours)
-                {
-                    var area = Cv2.ContourArea(contour);
-                    if (area > 7000)
+                    var watch = System.Diagnostics.Stopwatch.StartNew();
+                    using (var gray = Cv2.ImRead($@"C:\Users\Denis\source\repos\Алгоритмы для курсовой работы\algorithms\Images\Train\img{id}.jpg", ImreadModes.Grayscale))
                     {
-                        RotatedRect rect = Cv2.MinAreaRect(contour);
-                        Point2f[] vertices = rect.Points();
+                        Mat image = Cv2.ImRead($@"C:\Users\Denis\source\repos\Алгоритмы для курсовой работы\algorithms\Images\Train\img{id}.jpg", ImreadModes.Color);
 
-                        using (Graphics graphic = Graphics.FromImage(tempImage))
+                        Cv2.Resize(image, image, new OpenCvSharp.Size(1024, 512));
+                        Cv2.Resize(gray, gray, new OpenCvSharp.Size(1024, 512));
+
+                        /*using (new Window("image", image, WindowFlags.AutoSize))
                         {
-                            for (int i = 0; i < 4; i++)
-                                Cv2.Line(image, (OpenCvSharp.Point)vertices[i],
-                                                (OpenCvSharp.Point)vertices[(i + 1) % 4],
-                                                new Scalar(0, 255, 0));
-
-                            graphic.RotateTransform(rect.Angle);
-                            graphic.FillRectangle(brush, vertices[1].X, vertices[1].Y, rect.Size.Width, rect.Size.Height);                          
-                            
-                            tempImage.Save(@"absolute path", System.Drawing.Imaging.ImageFormat.Png);
+                            Cv2.WaitKey();
                         }
+                        using (new Window("gray image", gray, WindowFlags.AutoSize))
+                        {
+                            Cv2.WaitKey();
+                        }*/
+
+                        Mat blured = new Mat();
+                        Cv2.GaussianBlur(gray, blured, new OpenCvSharp.Size(17, 17), 0);
+                        /*using (new Window("blured image", blured, WindowFlags.AutoSize))
+                        {
+                            Cv2.WaitKey();
+                        }*/
+
+                        Mat threshold = new Mat();
+                        Cv2.AdaptiveThreshold(blured, threshold, 255,
+                                              AdaptiveThresholdTypes.GaussianC,
+                                              ThresholdTypes.Binary, 5, 0);
+                        /*using (new Window("adaptive threshold", threshold, WindowFlags.AutoSize))
+                        {
+                            Cv2.WaitKey();
+                        }*/
+
+                        Mat kernel = new Mat();
+                        Cv2.GetStructuringElement(MorphShapes.Rect, new OpenCvSharp.Size(5, 5));
+                        Mat dilate = new Mat();
+                        Cv2.Dilate(threshold, dilate, kernel);
+                        /*using (new Window("dilation", dilate, WindowFlags.AutoSize))
+                        {
+                            Cv2.WaitKey();
+                        }*/
+
+                        OpenCvSharp.Point[][] contours = new OpenCvSharp.Point[1][];
+                        HierarchyIndex[] temp = new HierarchyIndex[1];
+                        Cv2.FindContours(dilate, out contours, out temp,
+                                         RetrievalModes.External,
+                                         ContourApproximationModes.ApproxSimple);
+
+                        Bitmap tempImage = new Bitmap(gray.Width, gray.Height);
+                        for (int i = 0; i < gray.Width; i++)
+                            for (int j = 0; j < gray.Height; j++)
+                                tempImage.SetPixel(i, j, Color.Black);
+
+                        //tempImage.Save(@"C:\Users\Denis\source\repos\Алгоритмы для курсовой работы\algorithms\os.png", System.Drawing.Imaging.ImageFormat.Png);
+                        foreach (var contour in contours)
+                        {
+                            var area = Cv2.ContourArea(contour);
+                            if (area > 7000)
+                            {
+                                OpenCvSharp.RotatedRect rect = Cv2.MinAreaRect(contour);
+                                OpenCvSharp.Point2f[] vertices = rect.Points();
+
+                                using (Graphics graphic = Graphics.FromImage(tempImage))
+                                {
+                                    for (int i = 0; i < 4; i++)
+                                        Cv2.Line(image, (OpenCvSharp.Point)vertices[i],
+                                                        (OpenCvSharp.Point)vertices[(i + 1) % 4],
+                                                        new Scalar(0, 255, 0));
+                                    watch.Stop();
+
+                                    graphic.FillRectangle(brush, vertices[0].X, vertices[0].Y, rect.Size.Height, rect.Size.Width);
+                                    graphic.RotateTransform(rect.Angle);
+
+                                    //tempImage.Save(@"C:\Users\Denis\source\repos\Алгоритмы для курсовой работы\algorithms\os.png", System.Drawing.Imaging.ImageFormat.Png);
+                                }
+                            }
+                        }
+
+                        Mat mask = Cv2.ImRead($@"C:\Users\Denis\source\repos\Алгоритмы для курсовой работы\algorithms\Images\Annotation\Train\img{id}.png", ImreadModes.Unchanged);
+                        Cv2.Resize(mask, mask, new OpenCvSharp.Size(1024, 512));
+                        Metrics.Score scores = new Metrics.Score(OpenCvSharp.Extensions.BitmapConverter.ToBitmap(mask), tempImage);
+                        sheet.Cell($"A{row}").Value = "img" + id.ToString();
+                        sheet.Cell($"B{row}").Value = scores.GetAccuracy();
+                        sheet.Cell($"C{row}").Value = scores.GetF1();
+                        sheet.Cell($"D{row}").Value = watch.ElapsedMilliseconds;
+
+                        //Console.WriteLine($"Accuracy: {scores.GetAccuracy()}\nF1: {scores.GetF1()}\nTime: {watch.ElapsedMilliseconds} ms");
+
+                        /*using (new Window("result", image, WindowFlags.AutoSize))
+                        {
+                            Cv2.WaitKey();
+                        }*/
                     }
-                }
-
-                Mat mask = new Mat(@"absolute path");
-                Cv2.Resize(mask, mask, new OpenCvSharp.Size(1024, 512));
-                Metrics.Score scores = new Metrics.Score(OpenCvSharp.Extensions.BitmapConverter.ToBitmap(mask), tempImage);
-                Console.WriteLine($"Accuracy: {scores.GetAccuracy()}\nF1: {scores.GetF1()}");
-
-                using (new Window("result", image, WindowFlags.AutoSize))
-                {
-                    Cv2.WaitKey();
+                    row++;
                 }
             }
+            newExcelFile.SaveAs(@"absolute path");
         }
     }
 }
--- SecondFilter/SecondFilter.cs
+++ SecondFilter/SecondFilter.cs
@@ -1,6 +1,8 @@
 ﻿using System;
-using OpenCvSharp;
 using System.Drawing;
+using System.IO;
+using OpenCvSharp;
+using ClosedXML.Excel;
 
 namespace FirstFilter
 {
@@ -9,86 +11,108 @@ namespace FirstFilter
         static void Main(string[] args)
         {
             SolidBrush brush = new SolidBrush(Color.White);
+            IXLWorkbook newExcelFile = new XLWorkbook();
+            IXLWorksheet sheet = newExcelFile.Worksheets.Add("Second filter");
+            sheet.Cell("A1").Value = "Image";
+            sheet.Cell("B1").Value = "Accuracy";
+            sheet.Cell("C1").Value = "F1";
+            sheet.Cell("D1").Value = "Time";
 
-            using (var gray = Cv2.ImRead(@"absolute path", ImreadModes.Grayscale))
+            int row = 2;
+            for (short id = 11; id < 1539; id++)
             {
-                Mat image = Cv2.ImRead(@"absolute path", ImreadModes.Color);
-
-                Cv2.Resize(image, image, new OpenCvSharp.Size(1024, 512));
-                Cv2.Resize(gray, gray, new OpenCvSharp.Size(1024, 512));
-
-                using (new Window("image", image, WindowFlags.AutoSize))
+                if (File.Exists($@"C:\Users\Denis\source\repos\Алгоритмы для курсовой работы\algorithms\Images\Train\img{id}.jpg"))
                 {
-                    Cv2.WaitKey();
-                }
-                using (new Window("gray image", gray, WindowFlags.AutoSize))
-                {
-                    Cv2.WaitKey();
-                }
-
-                Mat threshold = new Mat();
-                Cv2.AdaptiveThreshold(gray, threshold, 255,
-                                      AdaptiveThresholdTypes.GaussianC,
-                                      ThresholdTypes.Binary, 23, 0);
-                using (new Window("adaptive threshold", threshold, WindowFlags.AutoSize))
-                {
-                    Cv2.WaitKey();
-                }
-
-                Mat kernel = new Mat();
-                Cv2.GetStructuringElement(MorphShapes.Rect, new OpenCvSharp.Size(5, 5));
-                Mat erode = new Mat();
-                Cv2.Erode(threshold, erode, kernel);
-                using (new Window("Erode", erode, WindowFlags.AutoSize))
-                {
-                    Cv2.WaitKey();
-                }
-
-                OpenCvSharp.Point[][] contours = new OpenCvSharp.Point[1][];
-                HierarchyIndex[] temp = new HierarchyIndex[1];
-                Cv2.FindContours(erode, out contours, out temp,
-                                 RetrievalModes.External,
-                                 ContourApproximationModes.ApproxSimple);
-
-                Bitmap tempImage = new Bitmap(gray.Width, gray.Height);
-                for (int i = 0; i < gray.Width; i++)
-                    for (int j = 0; j < gray.Height; j++)
-                        tempImage.SetPixel(i, j, Color.Black);
-
-                foreach (var contour in contours)
-                {
-                    var area = Cv2.ContourArea(contour);
-                    if (area > 500)
+                    var watch = System.Diagnostics.Stopwatch.StartNew();
+                    using (var gray = Cv2.ImRead($@"C:\Users\Denis\source\repos\Алгоритмы для курсовой работы\algorithms\Images\Train\img{id}.jpg", ImreadModes.Grayscale))
                     {
-                        OpenCvSharp.RotatedRect rect = Cv2.MinAreaRect(contour);
-                        OpenCvSharp.Point2f[] vertices = rect.Points();
+                        Mat image = Cv2.ImRead($@"C:\Users\Denis\source\repos\Алгоритмы для курсовой работы\algorithms\Images\Train\img{id}.jpg", ImreadModes.Color);
 
-                        using (Graphics graphic = Graphics.FromImage(tempImage))
+                        Cv2.Resize(image, image, new OpenCvSharp.Size(1024, 512));
+                        Cv2.Resize(gray, gray, new OpenCvSharp.Size(1024, 512));
+
+                        /*using (new Window("image", image, WindowFlags.AutoSize))
                         {
-                            for (int i = 0; i < 4; i++)
-                                Cv2.Line(image, (OpenCvSharp.Point)vertices[i],
-                                                (OpenCvSharp.Point)vertices[(i + 1) % 4],
-                                                new Scalar(0, 255, 0));
-
-                            graphic.RotateTransform(rect.Angle);
-                            graphic.FillRectangle(brush, vertices[1].X, vertices[1].Y, rect.Size.Width, rect.Size.Height);
-                            
-                            tempImage.Save(@"absolute path", System.Drawing.Imaging.ImageFormat.Png);
+                            Cv2.WaitKey();
                         }
-                        
+                        using (new Window("gray image", gray, WindowFlags.AutoSize))
+                        {
+                            Cv2.WaitKey();
+                        }*/
+
+                        Mat threshold = new Mat();
+                        Cv2.AdaptiveThreshold(gray, threshold, 255,
+                                              AdaptiveThresholdTypes.GaussianC,
+                                              ThresholdTypes.Binary, 23, 0);
+                        /*using (new Window("adaptive threshold", threshold, WindowFlags.AutoSize))
+                        {
+                            Cv2.WaitKey();
+                        }*/
+
+                        Mat kernel = new Mat();
+                        Cv2.GetStructuringElement(MorphShapes.Rect, new OpenCvSharp.Size(5, 5));
+                        Mat erode = new Mat();
+                        Cv2.Erode(threshold, erode, kernel);
+                        /*using (new Window("Erode", erode, WindowFlags.AutoSize))
+                        {
+                            Cv2.WaitKey();
+                        }*/
+
+                        OpenCvSharp.Point[][] contours = new OpenCvSharp.Point[1][];
+                        HierarchyIndex[] temp = new HierarchyIndex[1];
+                        Cv2.FindContours(erode, out contours, out temp,
+                                         RetrievalModes.External,
+                                         ContourApproximationModes.ApproxSimple);
+
+                        Bitmap tempImage = new Bitmap(gray.Width, gray.Height);
+                        for (int i = 0; i < gray.Width; i++)
+                            for (int j = 0; j < gray.Height; j++)
+                                tempImage.SetPixel(i, j, Color.Black);
+
+                        foreach (var contour in contours)
+                        {
+                            var area = Cv2.ContourArea(contour);
+                            if (area > 500)
+                            {
+                                OpenCvSharp.RotatedRect rect = Cv2.MinAreaRect(contour);
+                                OpenCvSharp.Point2f[] vertices = rect.Points();
+
+                                using (Graphics graphic = Graphics.FromImage(tempImage))
+                                {
+                                    for (int i = 0; i < 4; i++)
+                                        Cv2.Line(image, (OpenCvSharp.Point)vertices[i],
+                                                        (OpenCvSharp.Point)vertices[(i + 1) % 4],
+                                                        new Scalar(0, 255, 0));
+                                    watch.Stop();
+                                    
+                                    graphic.FillRectangle(brush, vertices[0].X, vertices[0].Y, rect.Size.Height, rect.Size.Width);
+                                    graphic.RotateTransform(rect.Angle);
+
+                                    tempImage.Save(@"C:\Users\Denis\source\repos\Алгоритмы для курсовой работы\algorithms\os.png", System.Drawing.Imaging.ImageFormat.Png);
+                                }
+
+                            }
+                        }
+
+                        Mat mask = Cv2.ImRead($@"C:\Users\Denis\source\repos\Алгоритмы для курсовой работы\algorithms\Images\Annotation\Train\img{id}.png", ImreadModes.Unchanged);
+                        Cv2.Resize(mask, mask, new OpenCvSharp.Size(1024, 512));
+                        Metrics.Score scores = new Metrics.Score(OpenCvSharp.Extensions.BitmapConverter.ToBitmap(mask), tempImage);
+                        sheet.Cell($"A{row}").Value = "img" + id.ToString();
+                        sheet.Cell($"B{row}").Value = scores.GetAccuracy();
+                        sheet.Cell($"C{row}").Value = scores.GetF1();
+                        sheet.Cell($"D{row}").Value = watch.ElapsedMilliseconds;
+
+                        //Console.WriteLine($"Accuracy: {scores.GetAccuracy()}\nF1: {scores.GetF1()}\nTime: {watch.ElapsedMilliseconds} ms");
+
+                        /*using (new Window("result", image, WindowFlags.AutoSize))
+                        {
+                            Cv2.WaitKey();
+                        }*/
                     }
-                }
-
-                Mat mask = new Mat(@"absolute path");
-                Cv2.Resize(mask, mask, new OpenCvSharp.Size(1024, 512));
-                Metrics.Score scores = new Metrics.Score(OpenCvSharp.Extensions.BitmapConverter.ToBitmap(mask), tempImage);
-                Console.WriteLine($"Accuracy: {scores.GetAccuracy()}\nF1: {scores.GetF1()}");
-
-                using (new Window("result", image, WindowFlags.AutoSize))
-                {
-                    Cv2.WaitKey();
+                    row++;
                 }
             }
+            newExcelFile.SaveAs(@"C:\Users\Denis\source\repos\Алгоритмы для курсовой работы\algorithms\second-filter.xlsx");
         }
     }
 }
